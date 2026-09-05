4b8e4d7eb621d1bc7f51ff240e6e5d9ade2037df
Yusuf Raji and Christian Jaeger <raji.yusuf234@gmail.com>
2026-09-05 15:50:47 +0200

    Initial working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use std::process::{Command, Stdio, exit};
+
+use anyhow::{Context, Result};
+use bstr::ByteSlice;
+use clap::Parser;
+use rand::Rng;
+
+#[derive(Parser)]
+struct Args {
+    name: String,
+
+    #[arg(required = true)]
+    email: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let output = Command::new("git")
+        .args(["config", "get", "--global", "user.name"])
+        .stderr(Stdio::inherit())
+        .output()
+        .context("getting user.name from git config")?;
+
+    if !output.status.success() {
+        exit(1)
+    }
+
+    if output.stdout.as_bstr().trim_ascii() == args.name.as_bytes() {
+        let mut rng = rand::thread_rng();
+        let emails = args.email;
+        let i: usize = rng.gen_range(0..emails.len());
+        let email = &emails[i];
+        let status = Command::new("git")
+            .args(["config", "set", "--global", "user.email", email])
+            .status()
+            .context("setting user.email of git config")?;
+        if !status.success() {
+            exit(1);
+        }
+    }
+
+    Ok(())
 }
